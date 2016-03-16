b4cf53bcd548a1169c8f96928d618ff86b42769c
Andrew R McHugh <arm5629@gmail.com>
2016-03-16 11:27:01 +0000

    Setup Grunt for livereload

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+     pkg: grunt.file.readJSON('package.json'),
+     watch: {
+       html: {
+        files: ['**.html'],
+        tasks: ['default'],
+        options: {
+         livereload: true,
+         spawn: false,
+      },
+   },
+},
+});
+
+  // Load all the plugins!
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
